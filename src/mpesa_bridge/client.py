e6a39c3bb38e714a8ec28e63e268edfd23a1397e
kahono0 <kahonojackson@gmail.com>
2023-08-17 01:18:40 +0300

    add small error handling

--- src/mpesa_bridge/client.py
+++ src/mpesa_bridge/client.py
@@ -9,49 +9,73 @@ class Client:
         self.base_url = base_url
 
     def request_payment(self, amount_in_cents, phone) -> dict:
-        client = GraphqlClient(endpoint=self.base_url)
-        query = """
-        mutation {
-            initateDeposit(input:{
-            amountInCents: %s,
-            userPhoneNumber: "%s"
-            }){
-            Txid
-            Status
+        if self.api_key == "" or self.base_url == "":
+            raise Exception("Please set the api key and base url")
 
+        try:
+            amount_in_cents = int(amount_in_cents)
+        except ValueError:
+            raise Exception("Amount must be an integer")
+        if amount_in_cents < 100:
+            raise Exception("You can only request amounts greater than 1 shilling")
+
+        try:
+            client = GraphqlClient(endpoint=self.base_url)
+            query = """
+            mutation {
+                initateDeposit(input:{
+                amountInCents: %s,
+                userPhoneNumber: "%s"
+                }){
+                Txid
+                Status
+
+                }
             }
-        }
-        """ % (amount_in_cents, phone)
+            """ % (amount_in_cents, phone)
 
-        # add headers
-        headers = {
+            # add headers
+            headers = {
                 "Authorization": "Bearer " + self.api_key,
                 "Content-Type": "application/json"
-        }
+            }
 
-        data = client.execute(query=query, headers=headers)
-        return data
+            data = client.execute(query=query, headers=headers)
+            return data
+        except:
+            raise Exception("Something went wrong while sending the request. Please confirm you have the right API key and base url. If the problem persists, please contact support")
+
 
     def fetch_transaction(self, transaction_id):
-        client = GraphqlClient(endpoint=self.base_url)
-        query = """
-        query {
-            fetchTransactionByTxId(input: "%s"){
-            id
-            amount
-            type
-            status
-            mpesaReference
-            }
-        }
-        """ % (transaction_id)
+        if self.api_key == "" or self.base_url == "":
+            raise Exception("Please set the api key and base url")
+        if transaction_id == "":
+            raise Exception("Please provide a transaction id")
 
-        # add headers
-        headers = {
+        try:
+            client = GraphqlClient(endpoint=self.base_url)
+            query = """
+            query {
+                fetchTransactionByTxId(input: "%s"){
+                id
+                amount
+                type
+                status
+                mpesaReference
+                }
+            }
+            """ % (transaction_id)
+
+            # add headers
+            headers = {
                 "Authorization": "Bearer " + self.api_key,
                 "Content-Type": "application/json"
-        }
+            }
 
-        data = client.execute(query=query, headers=headers)
+            data = client.execute(query=query, headers=headers)
 
-        return data
+            return data
+        except:
+            raise Exception("Something went wrong while sending the request. Please confirm you have the right API key and base url. If the problem persists, please contact support")
+
+
